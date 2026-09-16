ab7118a0e11eec38bfb6e7f54a50ad5000ce9912
Shravan Choudhary <smchoudhary19093@gmail.com>
2026-09-16 17:23:59 +0530

    style: format code for consistency and readability in route.ts

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,28 +1,28 @@
 import { prisma } from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(){
-    const posts=await prisma.post.findMany({
-        include:{
-            user:true
+export async function GET() {
+    const posts = await prisma.post.findMany({
+        include: {
+            user: true
         }
     })
 
     return Response.json(posts)
 }
 
-export async function POST(req:NextRequest){
-    const createpost= await req.json();
+export async function POST(req: NextRequest) {
+    const createpost = await req.json();
 
-    const post=await prisma.post.create({
-         data:{
-            content:createpost.content,
-            link:createpost.link,
-            tags:[createpost.tags ]
-         }
+    const post = await prisma.post.create({
+        data: {
+            content: createpost.content,
+            link: createpost.link,
+            tags: [createpost.tags]
+        }
     })
 
     return NextResponse.json({
-        message:"Post have been created"
+        message: "Post have been created"
     })
 }
